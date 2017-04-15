f56f28b47fbb8d567130a28f75120b19e12e14d0
Tim Lardner <tim@lardner.io>
2017-04-15 09:24:21 +0100

    Removed wait for workers, amended readme.

--- README.md
+++ README.md
@@ -5,4 +5,10 @@
     docker-compose build
     docker-compose up
 
+To run multiple celery clients, do:
+
+    docker-compose scale=N
+
+where N is the desired number of backend worker nodes.
+
 Then visit http://localhost:5000 for a demo
--- run_celery.sh
+++ run_celery.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-# wait for RabbitMQ server to start
-sleep 10
-
 cd WJ  
 
 # run Celery worker for our project myproject
-su -m wj -c "celery -A wj.celery worker"  
+su -m wj -c "celery -A wj.celery worker --loglevel INFO	"  
